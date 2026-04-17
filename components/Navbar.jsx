9ee402f4a0f367f18622b9321c4215522e88c49c
kingroyalhote-prog <king.royal.hote@gmail.com>
2026-04-16 22:31:30 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,26 @@
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <div className="nav-inner">
-        <Link href="/" className="brand">Lamoille High School</Link>
-        <Link href="/staff">Staff</Link>
+    <nav className="site-nav">
+      <div className="site-nav-inner">
+        <Link href="/" className="site-brand">
+          <Image
+            src="/images/lamoille-logo.png"
+            alt="Lamoille High School logo"
+            width={42}
+            height={42}
+          />
+          <span>Lamoille High School</span>
+        </Link>
+
+        <Link href="/about">About</Link>
+        <Link href="/announcements">Announcements</Link>
+        <Link href="/staff-directory">Staff Directory</Link>
         <Link href="/employment">Employment</Link>
-        <Link href="/admin">Administration</Link>
+        <Link href="/login">Login</Link>
       </div>
     </nav>
-  );
+  )
 }
