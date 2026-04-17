3c7cbc9d6905cd24260df2bf64c580f00c5620a5
kingroyalhote-prog <king.royal.hote@gmail.com>
2026-04-16 23:26:43 -0400

    Create loading.jsx

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <main className="loading-screen">
+      <div className="loading-inner">
+        <div className="loading-spinner" />
+        <h1>Lamoille High School</h1>
+        <p>Loading...</p>
+      </div>
+    </main>
+  )
+}
