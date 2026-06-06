1524040e10ec83bc2b97472e31ee7ffefc722959
kingroyalhote-prog <king.royal.hote@gmail.com>
2026-06-06 00:23:36 -0400

    Create layout.jsx

---
+++ app/maintenance/layout.jsx
@@ -0,0 +1,9 @@
+export default function MaintenanceLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  )
+}
