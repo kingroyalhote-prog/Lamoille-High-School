5b5109c8158ffc376778be09ea0ad2f9963636c4
kingroyalhote-prog <king.royal.hote@gmail.com>
2026-06-30 15:11:28 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,18 +28,18 @@ export default function Navbar() {
   }
 
   return (
- <header className="district-header">
+    <header className="district-header">
       <div className="district-topbar">
         <Link href="/" className="district-brand">
           <Image
             src="/images/lamoille-logo.png"
-            alt="Lamoille High School"
+            alt="Lamoille ISD"
             width={64}
             height={64}
           />
 
           <div>
-            <h1>Lamoille High School</h1>
+            <h1>Lamoille ISD</h1>
             <p>Where Ambition Meets Opportunity</p>
           </div>
         </Link>
@@ -64,8 +64,8 @@ export default function Navbar() {
       </div>
 
       <nav className="district-nav">
-        <Link href="/about">About</Link>
-        <Link href="/employment">Employment</Link>
+        <Link href="/about">About ISD</Link>
+        <Link href="/employment">Careers</Link>
         <Link href="/clubs">Clubs</Link>
         <Link href="/athletics">Athletics</Link>
         <Link href="/calendar">Calendar</Link>
